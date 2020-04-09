d33daa23129a8a5f085b54e9c26e4f1a93ce2a9c
xatikoo <60353161+xatikoo@users.noreply.github.com>
2020-04-09 20:13:36 +0500

    homework 4
    >>homework 4

---
+++ homework4/homework4/homework4.cpp
@@ -0,0 +1,211 @@
+﻿using namespace std;
+#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	//int sum1, sum2;
+	//int cif1, cif2, cif3, cif4, cif5, cif6;
+	//int ch;
+	//cout << "Введите шестизначное число-";
+	//cin >> ch;
+	//if (ch > 99999 && ch < 1000000)
+	//{
+	//	cif1 = ch / 100000;
+	//	cif2 = (ch % 100000) / 10000;
+	//	cif3 = ((ch % 100000) % 10000) / 1000;
+	//	cif4 = ((ch % 100000) % 10000) % 1000 / 100;
+	//	cif5 = ((ch % 100000) % 10000) % 1000 % 100 / 10;
+	//	cif6 = ((ch % 100000) % 10000) % 1000 % 100 % 10;
+	//	sum1 = cif1 + cif2 + cif3;
+	//	sum2 = cif4 + cif5 + cif6;
+	//	if (sum1 == sum2)
+	//		cout << ch << "-СЧАСТЛИВОЕ ЧИСЛО\n";
+	//	else if (sum1 != sum2)
+	//		cout << ch << "-НЕСЧАСТЛИВОЕ ЧИСЛО\n";
+	//}
+	//else 
+	//	cout << "ВВЕДИТЕ ДРУГОЕ ЧИСЛО\n";
+
+	//int a;
+	//int cif1, cif2, cif3, cif4;
+	//cout << "Введите четырехзначное число-";
+	//int ch;
+	//cin >> ch;
+	//if (ch > 999 || ch < 10000) {
+	//	cif1 = ch / 1000;
+	//	cif2 = ch % 1000 / 100;
+	//	cif3 = ch % 1000 % 100 / 10;
+	//	cif4 = ch & 100 % 100 % 10;
+	//	cout << cif2 << cif1 << cif4 << cif3;
+		// я понял что тут имеет ввиду числа местами поменять,
+		// но я в этом не уверен
+		// странное задание
+	//}
+	
+	//int cif1, cif2, cif3, cif4, cif5, cif6, cif7;
+	//cin >> cif1 >> cif2 >> cif3 >> cif4 >> cif5 >> cif6 >> cif7;
+	//for (int a = 0; a < 8; a++) {
+	//	cin >> a;
+	//	if (a > ) {
+	//		cout << a << "-самое большое число\n";
+	//	}
+		//if (cif2 > cif1, cif2, cif3, cif4, cif5, cif6, cif7) {
+		//	//cout << cif2 << "-самое большое число\n";
+		//}
+		//if (cif3 > cif1, cif2, cif3, cif4, cif5, cif6, cif7) {
+			//cout << cif3 << "-самое большое число\n";
+		//}
+	//}
+
+
+	//int ch = 0;
+	//int max = 0;
+
+	//	for (int i = 0; i < 7; i++) {
+	//		cin >> ch;
+	//		if (ch > max) {
+	//			max = ch;
+	//		}
+	//	}
+	//	cout << "Максимальное число-" << max;
+
+	//int kg; int km = 1, litr; int S1, S2; int t2, t1, t;
+	//cout << "Сколько кг груза\nдо 500кг\nдо 1000кг\nдо 1500кг\nдо 2000кг\nболее 2000кг самолет не поднимает\n\n";
+	//cin >> kg;
+	//system("cls");
+
+	//if (kg < 2000) {
+	//	cout << "Какое расстояние вам надо пролететь\nA-B\nB-C\n\n";
+	//	cin >> S1 >> S2;
+	//	cout << endl;
+	//	if (kg == 500) {
+	//		litr = 1;
+	//		t2 = S2 * (litr / km);
+	//		t1 = S1 * (litr / km);
+	//		t = t1 + t2;
+	//		cout << "нужно-" << t << " литров чтобы долететь из А в С\n";
+	//		cout << "нужно-" << t1 << " литров чтобы долететь из А в B\n";
+	//		cout << "нужно-" << t2 << " литров чтобы долететь из B в С\n";
+	//	}
+	//	if (kg == 1000) {
+	//		litr = 4;
+	//		t2 = S2 * (litr / km);
+	//		t1 = S1 * (litr / km);
+	//		t = t1 + t2;
+	//		cout << "нужно-" << t << " литров чтобы долететь из А в С\n";
+	//		cout << "нужно-" << t1 << " литров чтобы долететь из А в B\n";
+	//		cout << "нужно-" << t2 << " литров чтобы долететь из B в С\n";
+	//	}
+	//	if (kg == 1500) {
+	//		litr = 7;
+	//		t2 = S2 * (litr / km);
+	//		t1 = S1 * (litr / km);
+	//		t = t1 + t2;
+	//		cout << "нужно-" << t << " литров чтобы долететь из А в С\n";
+	//		cout << "нужно-" << t1 << " литров чтобы долететь из А в B\n";
+	//		cout << "нужно-" << t2 << " литров чтобы долететь из B в С\n";
+	//	}
+	//	if (kg == 2000) {
+	//		litr = 9;
+	//		t2 = S2 * (litr / km);
+	//		t1 = S1 * (litr / km);
+	//		t = t1 + t2;
+	//		cout << "нужно-" << t << " литров чтобы долететь из А в С\n";
+	//		cout << "нужно-" << t1 << " литров чтобы долететь из А в B\n";
+	//		cout << "нужно-" << t2 << " литров чтобы долететь из B в С\n";
+	//	}
+	//}
+	//else
+	//	cout << "более 2000кг самолет не поднимает";
+
+		//int day, day1, month, month1, year, year1, diffye, diffmon, diffmon1;
+		//cout << "Введите день первой даты: ";
+		//cin >> day;
+		//cout << "Введите месяц первой даты: ";
+		//cin >> month;
+		//cout << "Введите год первой даты: ";
+		//cin >> year;
+		//cout << "Введите день второй даты: ";
+		//cin >> day1;
+		//cout << "Введите месяц второй даты: ";
+		//cin >> month1;
+		//cout << "Введите год второй даты: ";
+		//cin >> year1;
+
+		//if (day <= 31 && day1 <= 31 && month <= 12 && month1 <= 12) 
+		//{
+		//	diffye = (year1 - year) * 365;
+
+		//	if 
+		//		(month > 8) diffmon = ((month - 1) * 30) + ((month - 1) / 2) + ((month - 1) % 2) - 1 + day;
+		//	else if 
+		//		(month > 2) diffmon = ((month - 1) * 30) + ((month - 1) / 2) + ((month - 1) % 2) - 2 + day;
+		//	else 
+		//		diffmon = ((month - 1) * 30) + ((month - 1) / 2) + ((month - 1) % 2) + day;
+
+		//	if 
+		//		(month1 > 8) diffmon1 = ((month1 - 1) * 30) + ((month1 - 1) / 2) + ((month1 - 1) % 2) - 1 + day1;
+		//	else if 
+		//		(month1 > 2) diffmon1 = ((month1 - 1) * 30) + ((month1 - 1) / 2) + ((month1 - 1) % 2) - 2 + day1;
+		//	else 
+		//		diffmon1 = ((month1 - 1) * 30) + ((month1 - 1) / 2) + ((month1 - 1) % 2) + day1;
+
+		//	if (diffye > 0) {
+		//		(diffye -= diffmon) += diffmon1;
+		//	}
+		//	else {
+		//		diffye = diffmon1 - diffmon;
+		//	}
+
+		//
+		//	if	(((year1 - year) / 2) > 1 && month1 > 2) diffye += ((year1 - year) / 4) + 1;
+		//		cout << "Количество дней между этими датами-" << diffye << "\n";
+		//}
+		//else  
+		//	cout << "Вы ввели некорректные даты!\n";
+	
+	//int zp1 = 0;
+	//int zp2 = 0;
+	//int zp3 = 0;
+	//int meneger1, meneger2, meneger3;
+	//cout << "Введите уровень продаж\n1 менеджер\n2 менеджер\n3 менеджерг\n";
+	//cin >> meneger1 >> meneger2 >> meneger3;
+	//system("cls");
+	//if (meneger1 < 500)
+	//	zp1 = 200 + meneger1 / 100 * 3;
+	//if (meneger1 > 500 || meneger1 < 1000)
+	//	zp1 = 200 + meneger1 / 100 * 5;
+	//if (meneger1 < 1000)
+	//	zp1 = 200 + meneger1 / 100 * 8;
+
+	//if (meneger2 < 500)
+	//	zp2 = 200 + meneger2 / 100 * 3;
+	//if (meneger2 > 500 || meneger2 < 1000)
+	//	zp2 = 200 + meneger2 / 100 * 5;
+	//if (meneger2 < 1000)
+	//	zp2 = 200 + meneger2 / 100 * 8;
+	//
+	//if (meneger3 < 500)
+	//		zp3 = 200 + meneger3 / 100 * 3;
+	//if (meneger3 > 500 || meneger3 < 1000)
+	//	zp3 = 200 + meneger3 / 100 * 5;
+	//if (meneger1 < 1000)
+	//	zp3 = 200 + meneger3 / 100 * 8;
+	//	
+	//cout << "зарплата 1 менеджера-" << zp1 << endl;
+	//cout << "зарплата 2 менеджера-" << zp2 << endl;
+	//cout << "зарплата 3 менеджера-" << zp3 << endl << endl;
+
+	//if (zp1 > zp2 && zp1 > zp3)
+	//	cout << "зарплата 1 менеджера самая большая\n";
+
+	//else if (zp2 > zp1 && zp2 > zp3)
+	//	cout << "зарплата 2 менеджера самая большая\n";
+
+	//else if (zp3 > zp2 && zp3 > zp1)
+	//	cout << "зарплата 3 менеджера самая большая\n";
+	//			
+
+}
+
