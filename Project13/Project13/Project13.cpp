27ab8ad232ca855c4f5d6c7dabe16a59151a4edb
xatikoo <60353161+xatikoo@users.noreply.github.com>
2020-04-30 19:14:02 +0500

    строка
    ..

---
+++ Project13/Project13/Project13.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <cstring>
+#include <cstdio>
+using namespace std;
+
+
+
+int main()
+{
+    const char* str = "Белеет парус одинокий в тумане моря голубом!";
+    stringstream strin;
+    strin << str;
+    string strout = "";
+    string word;
+    while (strin >> word)
+    {
+        if (word.length() >= 5) {
+            std::reverse(word.begin(), word.end());
+        }
+        strout += word + ' ';
+    }
+    strout.pop_back();
+    cout << str << endl;
+    cout << strout.c_str();
+}
